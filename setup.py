4cb6d85f3bc7cfa032d39ebe6fa92825c324689e
Dean Gardiner <me@dgardiner.net>
2016-05-05 16:03:31 +1200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='oem-format-minimize-json',
+    version='1.0.0',
+
+    author="Dean Gardiner",
+    author_email="me@dgardiner.net",
+
+    install_requires=[
+        'oem-framework>=1.0.0',
+        'oem-format-minimize>=1.0.0',
+        'oem-format-json>=1.0.0'
+    ]
+)
